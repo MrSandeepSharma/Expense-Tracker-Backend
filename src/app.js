8f64aa8c4a089f64a6040987ffd47575d1beedb9
Sandeep <sandipsharma2445@gmail.com>
2024-05-18 10:26:42 +0530

    user auth routes created

---
+++ src/app.js
@@ -0,0 +1,9 @@
+import express from "express";
+
+import userRouter from "./routes/user.routes.js";
+
+const app = express();
+
+app.use("api/v1/auth", userRouter)
+
+export { app };
--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
-import express from "express";
-
-const app = express();
+import { app } from "./app.js"
 
 app.get("/", (req, res) => {
     res.send("<h1>Project Setup</h1>")
