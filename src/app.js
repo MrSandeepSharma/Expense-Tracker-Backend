552a58e393a3e302890e83fa5633fa35bcf9e5d9
Sandeep <sandipsharma2445@gmail.com>
2024-05-19 14:22:26 +0530

    cors port change

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ import { jwtAuthMiddleware } from "./middlewares/jwt.middlewares.js";
 const app = express();
 
 const corsOptions = {
-    origin: "http://localhost:3000",
+    origin: "http://localhost:5173",
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
     optionsSuccessStatus: 204,
